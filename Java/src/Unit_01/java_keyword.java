161b099768e81d6bd6fd850b180a8f6d2047b0c1
Hp <Hp@ShabbarRizvi>
2022-03-25 16:21:14 +0530

    Done

---
+++ Java/src/Unit_01/java_keyword.java
@@ -0,0 +1,41 @@
+package Unit_01;
+/* 50 such keywords in java.
+ * 48+2 in which 48 use and 2 for future use[goto*,const**].
+ * 1.2v = assert**
+ * 1.4v = strictfp***
+ * 5.0v = enum****
+ * keywords cannot be used as identifiers in java.
+ * 1.object
+ * 2.variable name
+ * 3.function
+ * 4.class etc.
+ */
+	public class java_keyword {
+		public static void main(string[] args)
+		{
+			//variable name
+			int double = 12;
+			
+			//function  name
+			int double() {
+				System.out.println();
+				return 0;
+			}
+			
+			ABC int = new ABC();
+		}
+	}
+//class name can never be any keywords in java.
+		class int{
+			void display() {
+				System.out.println("another class");
+			}
+		}
+//object
+		class ABC{
+			void display() {
+				System.out.println("another class");
+//object name can never be any keywords.
+			}
+		}
+
